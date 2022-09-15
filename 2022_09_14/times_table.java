f7f0494374ba686eea8cfc071078a61b3a81240e
Trisha Bhatawdekar <t@ameyab.com>
2022-09-14 20:22:17 -0700

    Homework for 09.14.2022

---
+++ 2022_09_14/times_table.java
@@ -0,0 +1,31 @@
+public class Main {
+  public static void main(String[] args) {
+    //1
+    //12
+    //123
+    //1234
+    //12345
+    
+   
+    
+    // int, float, string
+    
+    for(int i=1; i<=5; i++){
+    	//i --> 1, 2, 3, 4, 5
+		
+        
+        for (int j=1; j<=10; j++){
+        	// j --> 1, 2, 3, 4 ..., 9, 10
+            
+            // 1 x 1 = 1
+            // 1 x 2 = 2
+            // i x j = i*j 
+        	System.out.println(i + " x " + j + " = " + i*j);
+        }
+        
+        System.out.print("\n");
+        
+        
+    }
+  }
+}
